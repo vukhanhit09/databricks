91b268a1296d623fab5871bc2bd5ee767472de33
vukhanhit09 <khanhvv.ncthtb@gmail.com>
2026-04-07 13:52:32 +0000

    init

--- Job Pipeline/pl_UC2/transformations/bronze/bronze_cdr_dt_dim.sql
+++
@@ -1,6 +0,0 @@
--- Bronze layer: Ingest calendar date dimension data
-CREATE OR REFRESH MATERIALIZED VIEW bronze_cdr_dt_dim
-COMMENT "Calendar date dimension with fiscal and calendar attributes"
-AS
-SELECT *
-FROM mb_poc.gold.cdr_dt_dim
--- Job Pipeline/pl_UC2/transformations/bronze/bronze_crb.sql
+++
@@ -1,6 +0,0 @@
--- Bronze layer: Ingest raw data from crb table
-CREATE OR REFRESH MATERIALIZED VIEW bronze_crb
-COMMENT "Raw CRB transaction data with balance amounts and dimension groups"
-AS
-SELECT *
-FROM mb_poc.data_raw.crb
